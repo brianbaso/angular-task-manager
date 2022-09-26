630830bb65ab6756df2ccb51958587deb2419505
“brianbaso” <brianbaso@gmail.com>
2022-09-26 17:51:51 -0400

    fake backend server, delete task

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {Task} from '../../Task'
 import {faTimes} from '@fortawesome/free-solid-svg-icons'
 
@@ -9,6 +9,7 @@ import {faTimes} from '@fortawesome/free-solid-svg-icons'
 })
 export class TaskItemComponent implements OnInit {
   @Input() task!: Task
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter<Task>()
   faTimes = faTimes;
 
   constructor() {
@@ -17,4 +18,7 @@ export class TaskItemComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onDelete(task: Task) {
+    this.onDeleteTask.emit(task);
+  }
 }
